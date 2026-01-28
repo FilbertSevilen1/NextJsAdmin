ef6458234260cf362e8cf3dc3a55738eecd0948a
Ignatius Filbert Sevilen <sevilenfilbert@gmail.com>
2026-01-28 15:58:20 +0700

    [UPDATE] Notification Policy

--- src/app/module-testing/notification/page.tsx
+++ src/app/module-testing/notification/page.tsx
@@ -3,22 +3,21 @@
 import { useToast } from "@/components/context/ToastContext"
 
 export default function DialogTestPage() {
-    const { toast } = useToast();
+  const { toast } = useToast()
 
-    const sendNotification = async () => {
-        if (Notification.permission !== 'granted') {
-            await Notification.requestPermission()
-        }
+  const sendNotification = async () => {
+    const permission = await Notification.requestPermission()
+    if (permission !== "granted") return
 
-        new Notification('Hello', {
-            body: 'Membara Portoku'
-        })
-        toast.success("Notif Sent Successfully!!")
-    }
+    new Notification("Hello", { body: "Membara Portoku" })
+    toast.success("Notif Sent Successfully!!")
+  }
 
-    return (
-        <div className="grid m-8 gap-4">
-            <button className="btn btn-info w-full" onClick={() => sendNotification()}>Push Notification</button>
-        </div >
-    )
+  return (
+    <div className="grid m-8 gap-4">
+      <button className="btn btn-info w-full" onClick={sendNotification}>
+        Push Notification
+      </button>
+    </div>
+  )
 }
